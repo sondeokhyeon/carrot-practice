b16104228c2b9abccba0c9c5267fe9b62fa6c74f
sondeokhyeon <sondeokhyeon@gmail.com>
2022-06-15 18:39:44 +0900

    🚸  Link 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,23 +56,25 @@ const Home: NextPage = () => {
           </a>
         </Link>
       ))}
-      <button className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-3 text-white shadow-xl hover:bg-orange-500  cursor-pointer transition-colors">
-        <svg
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
-      </button>
+      <Link href="/items/upload">
+        <button className="fixed bottom-24 right-5 bg-orange-400 rounded-full p-3 text-white shadow-xl hover:bg-orange-500  cursor-pointer transition-colors">
+          <svg
+            className="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </button>
+      </Link>
     </div>
   );
 };
