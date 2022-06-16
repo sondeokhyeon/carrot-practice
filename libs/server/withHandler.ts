36d5bcfa0116af68878d9f2f6a94cfd698a10144
sondeokhyeon <sondeokhyeon@gmail.com>
2022-06-17 00:54:15 +0900

    router 적용

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+
+enum HttpMethod {
+  get = "GET",
+  post = "POST",
+  delete = "DELETE",
+}
+
+export default function withHandler(method: HttpMethod, fn: NextApiHandler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method !== method) {
+      return res.status(405).end();
+    }
+    try {
+      await fn(req, res);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
+  };
+}
+
+export { HttpMethod };
